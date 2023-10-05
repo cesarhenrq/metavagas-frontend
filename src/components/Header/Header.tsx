dd7f5a2cd8fcb7f20b9232d80c6e0f69f10f2ff3
cesarhenrq <cesaarhvb@gmail.com>
2023-10-05 19:38:58 -0300

    feat: add a divisor to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,32 +8,35 @@ const Header = () => {
     console.log('first');
   };
   return (
-    <S.Header>
-      <div className="text-container">
-        <Text label="meta" fontColor="white" fontSize="large" />
-        <Text
-          label="vagas"
-          fontColor="yellow"
-          fontStyle="italic"
-          fontSize="large"
-          fontWeight="600"
-        />
-      </div>
-      <div className="button-container">
-        <Button
-          label="Entrar"
-          onClick={dummy}
-          backgroundColor="purple-dark"
-          fontColor="white"
-          borderColor="white"
-        />
-        <Button
-          label="Cadastrar-se gratuitamente"
-          onClick={dummy}
-          borderColor="yellow"
-        />
-      </div>
-    </S.Header>
+    <>
+      <S.Header data-cy="header">
+        <div className="text-container">
+          <Text label="meta" fontColor="white" fontSize="large" />
+          <Text
+            label="vagas"
+            fontColor="yellow"
+            fontStyle="italic"
+            fontSize="large"
+            fontWeight="600"
+          />
+        </div>
+        <div className="button-container">
+          <Button
+            label="Entrar"
+            onClick={dummy}
+            backgroundColor="purple-dark"
+            fontColor="white"
+            borderColor="white"
+          />
+          <Button
+            label="Cadastrar-se gratuitamente"
+            onClick={dummy}
+            borderColor="yellow"
+          />
+        </div>
+      </S.Header>
+      <S.Line />
+    </>
   );
 };
 
