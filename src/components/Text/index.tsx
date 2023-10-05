d11dced1f42985278e56b8def59dd95da3b0ebf5
cesarhenrq <cesaarhvb@gmail.com>
2023-10-04 22:05:41 -0300

    refactor: add className and data-cy

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,7 +1,11 @@
 import * as S from './styles';
 
 const Text = (props: TextProps) => {
-  return <S.Text {...props}>{props.label}</S.Text>;
+  return (
+    <S.Text {...props} className="text" data-cy="text">
+      {props.label}
+    </S.Text>
+  );
 };
 
 export default Text;
