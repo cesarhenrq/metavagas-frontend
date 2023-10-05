7252186683a6f3862645096a84725e34242f2357
cesarhenrq <cesaarhvb@gmail.com>
2023-10-05 16:13:35 -0300

    test: footer component with jest

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react';
+
+import Footer from './Footer';
+
+describe('<Footer />', () => {
+  beforeEach(() => {
+    render(<Footer />);
+  });
+
+  it('should render the footer', () => {
+    screen.getByTestId('footer');
+  });
+
+  it('should render the line', () => {
+    screen.getByTestId('line');
+  });
+
+  it('should render the logo', () => {
+    screen.getByTestId('logo');
+  });
+
+  it('should render contact info', () => {
+    screen.getByTestId('contact-info');
+  });
+
+  it('should render text "Contato e Endereço"', () => {
+    screen.getByText('Contato e Endereço');
+  });
+
+  it('should render text "4003-5442"', () => {
+    screen.getByText('4003-5442');
+  });
+
+  it('should render adress info', () => {
+    screen.getByTestId('contact-info-adress');
+  });
+
+  it('should render the phone icon', () => {
+    screen.getByTestId('phone-icon');
+  });
+
+  it('should render the email icon', () => {
+    screen.getByTestId('email-icon');
+  });
+
+  it('should render the location icon', () => {
+    screen.getByTestId('location-icon');
+  });
+
+  it('should render auth button', () => {
+    screen.getByTestId('auth-buttons');
+  });
+});
