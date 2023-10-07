cd005aac1ff560584c3ee5310dc9da5759fe3d00
cesarhenrq <cesaarhvb@gmail.com>
2023-10-07 17:57:11 -0300

    test: add filters provider to mount method

--- src/components/SearchBar/SearchBar.cy.tsx
+++ src/components/SearchBar/SearchBar.cy.tsx
@@ -1,8 +1,23 @@
 import SearchBar from './SearchBar';
 
+import GlobalStyle from '@styles/global';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import FiltersProvider from '@contexts/filters';
+
 describe('<SearchBar />', () => {
   it('should not render with text "Buscas mais recentes" when search some location', () => {
-    cy.mount(<SearchBar />);
+    cy.mount(
+      <>
+        <GlobalStyle />
+        <FiltersProvider>
+          <BrowserRouter>
+            <SearchBar />
+          </BrowserRouter>
+        </FiltersProvider>
+      </>,
+    );
 
     cy.get('input[placeholder="Localização"]').type('test');
 
