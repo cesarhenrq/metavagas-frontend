456a38b17d7045434c6e617e6642f53505b4c4c7
cesarhenrq <cesaarhvb@gmail.com>
2023-10-09 12:14:40 -0300

    test: remove some assertions that should be e2e tests

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { expect, jest } from '@jest/globals';
 import '@testing-library/jest-dom/extend-expect';
-import userEvent from '@testing-library/user-event';
 
 import SearchBar from './SearchBar';
 
@@ -31,22 +30,5 @@ describe('<SearchBar />', () => {
 
   it('should render with the text "Onde?"', () => {
     (expect as any)(container).toHaveTextContent('Onde?');
-  });
-
-  it('should not render with the text "Buscas mais recentes:"', () => {
-    (expect as any)(container).not.toHaveTextContent('Buscas mais recentes:');
-  });
-
-  it('should render with text "Buscas mais recentes:" when search some tech or position', async () => {
-    const input = screen.getByPlaceholderText(
-      'Cargo, tecnologia ou palavra-chave',
-    );
-
-    const button = screen.getByText('Buscar vagas');
-
-    await userEvent.type(input, 'test');
-    await userEvent.click(button);
-
-    (expect as any)(container).toHaveTextContent('Buscas mais recentes:');
   });
 });
