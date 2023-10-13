81e6621e5315288e21bd8a808b66369f34d7ae3c
cesarhenrq <cesaarhvb@gmail.com>
2023-10-13 16:19:23 -0300

    feat: add styles to blur and centralize the redirect button

--- src/components/Chart/styles.ts
+++ src/components/Chart/styles.ts
@@ -1,14 +1,19 @@
 import styled from 'styled-components';
 
-export const Chart = styled.div`
+type ChartProps = {
+  isLogged: boolean;
+};
+
+export const Chart = styled.div<ChartProps>`
   display: flex;
   flex-direction: column;
   padding: 1.5rem 0rem;
-  width: 50%;
+  width: 100%;
   height: 269px;
   background: #f3f3f3;
   border-radius: 16px;
   border: 1px #ecf1f4 solid;
+  filter: ${props => (props.isLogged ? 'blur(0px)' : 'blur(3.5px)')};
 
   .text {
     justify-content: flex-start;
@@ -26,5 +31,18 @@ export const Chart = styled.div`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+  }
+`;
+
+export const ChartWrapper = styled.div`
+  position: relative;
+
+  button {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 300px;
+    z-index: 1;
   }
 `;
