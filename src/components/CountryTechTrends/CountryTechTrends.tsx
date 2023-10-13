dc3d8c536976a369b3f19cea6c32368213ff2461
cesarhenrq <cesaarhvb@gmail.com>
2023-10-13 16:20:50 -0300

    feat: country tech trends chart component

---
+++ src/components/CountryTechTrends/CountryTechTrends.tsx
@@ -0,0 +1,44 @@
+import Chart from '@components/Chart';
+
+import useResource from '@hooks/useResource';
+import useFetchResource from '@hooks/useFetchResource';
+
+const CountryTechTrends = () => {
+  const [vacancies, vacanciesService] = useResource<Vacancy>('vacancies');
+
+  useFetchResource(vacanciesService);
+
+  const label = (
+    <span>
+      Tecnologias mais procuradas em <span>Brasil</span>
+    </span>
+  );
+
+  const allTechnologies = vacancies.reduce<string[]>((acc, vacancy) => {
+    vacancy.technologies.forEach(tech => {
+      acc.push(tech.tecName);
+    });
+    return acc;
+  }, []);
+
+  const technologyCount = allTechnologies.reduce<{ [key: string]: number }>(
+    (acc, techName) => {
+      acc[techName] = (acc[techName] || 0) + 1;
+      return acc;
+    },
+    {},
+  );
+
+  const mostUsedTechnologies = Object.keys(technologyCount).map(techName => ({
+    name: techName,
+    qty: technologyCount[techName],
+  }));
+
+  mostUsedTechnologies.sort((a, b) => b.qty - a.qty);
+
+  const top5Technologies = mostUsedTechnologies.slice(0, 5);
+
+  return <Chart label={label} data={top5Technologies} />;
+};
+
+export default CountryTechTrends;
